5b8ad6f6c50dfd7f9a7c71d2d4dd6e6725cada03
Robert McKee <rcmckee@users.noreply.github.com>
2022-06-24 12:23:39 -0500

    trying my own and following solutions.

--- ds_algos_primer/python/linked_lists_part_1.py
+++ ds_algos_primer/python/linked_lists_part_1.py
@@ -23,6 +23,8 @@ class SinglyLinkedList:
     class _SinglyLinkedListNode:
         def __init__(self, val=0):
             # INSERT YOUR CODE HERE
+            self.value = val
+            self.next = None
 
     """
     Constructor
@@ -31,7 +33,10 @@ class SinglyLinkedList:
     Space Complexity:
     """
     def __init__(self):
-        # INSERT YOUR CODE HERE
+        # INSERT YOUR CODE HERE # this is different from the above lines 23 - 27 because that was just a node.
+        # this is for the entire list.
+        self.head = None
+        self.length = 0
 
     """
     Insert new node at the head of the list
@@ -41,6 +46,12 @@ class SinglyLinkedList:
     """
     def insert(self, n: int):
         # INSERT YOUR CODE HERE
+        # make a new node, then make it the head
+        new_Node =  self._SinglyLinkedListNode(n)
+        new_Node.next = self.head. # you need to point the new node to the current head before chaning the head
+        self.head = new_head
+        
+        self.length = self.length + 1
 
     """
     Delete the first occurrence of n from the list
@@ -50,7 +61,29 @@ class SinglyLinkedList:
     """
     def delete(self, n: int) -> bool:
         # INSERT YOUR CODE HERE
-
+        
+        # find the node to delete
+        
+        # once it is found, connect the previous node to the node on the other side of the node I am deleting.
+        # because this is a singly linked list without a pointer to the previous node, we need to create one and keep track of it.
+        prev = None
+        curr = self.head
+        
+        while curr:
+            if not prev:
+                #account for first node in the list
+                if curr.value == prev:
+                    self.head = curr.next
+                else:
+                    prev.next = curr.next
+                    
+                    self.length = self.length - 1
+                    
+                    return True
+            
+            prev = curr
+            curr = curr.next
+        return False
     """
     Return the number of items in the list
 
